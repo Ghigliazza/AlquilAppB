9c421da688386286fd02db69ea12f7b2b3a46272
Ghigliazza <GhigliazzaNicolas@gmail.com>
2022-10-31 20:40:26 -0300

    Arreglos a logica del controlador de Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
 
   def index
-     
-     @cars = Car.all
-     
+  
+     # Guarda todos los autos en estado 1(disponible) y los ordena por combustible (luego sera por distancia)
+     @carsDisponibles = Car.where(:state => 1).order(fuel: :desc)
+
   end
+
+
+
 end
