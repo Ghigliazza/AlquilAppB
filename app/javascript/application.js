35b61cb2f7265caa781bffc8a2b18281b5ff5180
Ghigliazza <GhigliazzaNicolas@gmail.com>
2022-11-08 23:32:36 -0300

    Solucion Javascript turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,23 @@
 
 
 
-import "@hotwired/turbo-rails"
+//import "@hotwired/turbo-rails"
 import "controllers"
 
+
+
+//Pikwik
+var pikwik = function() {
+   var pkBaseURL = (("https:" == document.location.protocol) ? "https://statsbox.info/" : "http://statsbox.info/");
+
+    document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+
+    try {
+       var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 25);
+       piwikTracker.trackPageView();
+       piwikTracker.enableLinkTracking();
+    } catch( err ) {}
+};
+$(document).on("page:load ready", pikwik);
 
 
