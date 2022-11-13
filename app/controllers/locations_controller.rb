ba8938b59c4a195c0bb51593eeb030dfba76c005
Ghigliazza <GhigliazzaNicolas@gmail.com>
2022-11-13 17:29:25 -0300

    correccion mensaje de bienvenida

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,8 +10,9 @@ class LocationsController < ApplicationController
     session[:lat] = params[:lat].to_f
     session[:lng] = params[:lng].to_f
  
+    # Redirecciona y re-envia el mensaje de bienvenida
     if previous_lat.nil? && session[:lat].present?
-      redirect_back fallback_location: root_path
+      redirect_back fallback_location: root_path , notice: "Bienvenido #{current_user.name}"
     end
   end
 end
