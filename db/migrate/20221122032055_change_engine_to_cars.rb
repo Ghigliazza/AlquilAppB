e00ad3051cd7e3b7b25eb0c23139537de751a747
Joaquin Montorsi <joaquinmontorsi@gmail.com>
2022-11-22 00:42:11 -0300

    afinando detalles extender cancelar y finalizar alquiler

---
+++ db/migrate/20221122032055_change_engine_to_cars.rb
@@ -0,0 +1,17 @@
+class ChangeEngineToCars < ActiveRecord::Migration[7.0]
+  def self.up
+    change_table :cars do |t|
+      t.change :engine, :boolean, default: :false
+    end
+  end
+  
+  def self.down
+    change_table :cars do |t|
+      t.change :engine, :booleant
+    end
+  end
+
+  def change
+    add_column :cars, :turn_on, :boolean, default: :false
+  end
+end
