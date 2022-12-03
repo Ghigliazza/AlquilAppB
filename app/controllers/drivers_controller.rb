6bdadeab88633b5c103afb87342fe8fe6f468735
Ghigliazza <GhigliazzaNicolas@gmail.com>
2022-12-03 19:21:35 -0300

    Visualizar balance total

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,6 +2,8 @@ class DriversController < ApplicationController
 
   def index
 
+    calculate_total_balance
+
   	 if !current_user.admin? && !current_user.supervisor?
   	 	redirect_to "/search", alert: "No tienes permiso para ver esta pagina!"
   	 end
@@ -52,6 +54,32 @@ class DriversController < ApplicationController
      end
 
   end
+
+  #Calcula el total de balance positivo y negativo en el sistema
+  def calculate_total_balance
+   @total_positive = 0
+   @total_negative = 0
+
+   if User.where(:rol => :driver).any?
+      User.where(:rol => :driver).each do |u| 
+         if u.balance > 0
+            @total_positive = @total_positive + u.balance
+         else
+            @total_negative = @total_negative - u.balance
+         end
+      end
+   end
+   if User.where(:rol => :suspended_driver).any?
+      User.where(:rol => :suspended_driver).each do |u| 
+         if u.balance > 0
+            @total_positive = @total_positive + u.balance
+         else
+            @total_negative = @total_negative - u.balance
+         end
+      end 
+   end
+  end
+  helper_method :calculate_total_balance
 
 
 end
