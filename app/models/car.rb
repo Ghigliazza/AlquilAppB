5ac9f00027b71d637d130793d1503f8fabdc6beb
Ghigliazza <GhigliazzaNicolas@gmail.com>
2022-12-06 22:58:34 -0300

    Cambios al formulario y validaciones autos

--- app/models/car.rb
+++ app/models/car.rb
@@ -19,4 +19,27 @@ class Car < ApplicationRecord
 	scope :colors, 		->(color){ where(color: color) }
 	scope :doors, 		->(doors){ where(doors: doors) }
 	scope :seats, 		->(seats){ where(seats: seats) }
+
+	validate :validate_doors
+	validate :validate_license
+	validate :validate_seats
+
+	def validate_doors
+      if !(doors > 0) || (doors > 6)
+          errors.add(:amount, 'Numero de puertas invalido')
+      end
+    end
+
+    def validate_license
+      if (Car.where(license:license).any?)
+          errors.add(:amount, 'La patente ya existe en el sistema')
+      end
+    end
+
+    def validate_seats
+      if !(seats > 0) || (seats > 10)
+          errors.add(:amount, 'Numero de asientos invalido')
+      end
+    end
+
 end
